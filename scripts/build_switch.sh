3a83dbed4a3eb05303a1346c086421f7e1c13511
PoloNX <geyrecharlie@gmail.com>
2024-01-05 14:25:15 +0000

    update build_switch

--- scripts/build_switch.sh
+++ scripts/build_switch.sh
@@ -3,5 +3,5 @@ set -e
 
 BUILD_DIR=cmake-build-switch
 
-cmake -B ${BUILD_DIR} -DCMAKE_BUILD_TYPE=Release -DBUILTIN_NSP=ON -DPLATFORM_SWITCH=ON -DCMAKE_BUILD_TYPE=Debug
+cmake -B ${BUILD_DIR} -DCMAKE_BUILD_TYPE=Release -DBUILTIN_NSP=ON -DPLATFORM_SWITCH=ON
 make -C ${BUILD_DIR} UpcomingSwitchGames.nro -j$(nproc)
